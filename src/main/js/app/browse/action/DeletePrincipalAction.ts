0c9db9364aac3e530db034d254493b50facaca48
Taukachou Mikita <edloidas@gmail.com>
2017-08-07 12:46:04 +0300

    Status message when deleting multiple users should say how many are deleted #5388
    Replaced notification message for multiple deletion.

--- src/main/js/app/browse/action/DeletePrincipalAction.ts
+++ src/main/js/app/browse/action/DeletePrincipalAction.ts
@@ -2,7 +2,6 @@ import '../../../api.ts';
 import {UserItemsTreeGrid} from '../UserItemsTreeGrid';
 import {UserTreeGridItem, UserTreeGridItemType} from '../UserTreeGridItem';
 import {DeletePrincipalRequest} from '../../../api/graphql/principal/DeletePrincipalRequest';
-import {DeleteUserStoreRequest} from '../../../api/graphql/userStore/DeleteUserStoreRequest';
 
 import Action = api.ui.Action;
 import DeletePrincipalResult = api.security.DeletePrincipalResult;
@@ -51,7 +50,9 @@ export class DeletePrincipalAction
 
                             if (results.length > 0) {
                                 const keys = results.filter(result => result.isDeleted()).map(result => result.getPrincipalKey());
-                                const msg = i18n(`notify.delete.principal.${keys.length === 1 ? 'single' : 'multiple'}`, keys.join(', '));
+                                const msg = keys.length === 1 ?
+                                            i18n('notify.delete.principal.single', keys[0]) :
+                                            i18n('notify.delete.principal.multiple', keys.length);
 
                                 api.notify.showFeedback(msg);
                                 api.security.UserItemDeletedEvent.create().setPrincipals(principalItems).build().fire();
@@ -60,16 +61,16 @@ export class DeletePrincipalAction
                 }
 
                 if (userStoreKeys && userStoreKeys.length > 0) {
-                    new DeleteUserStoreRequest()
+                    new api.security.DeleteUserStoreRequest()
                         .setKeys(userStoreKeys)
-                        .sendAndParse()
-                        .done((results: DeleteUserStoreResult[]) => {
+                        .send()
+                        .done((jsonResponse: api.rest.JsonResponse<any>) => {
+                            let json = jsonResponse.getJson();
 
-                            if (results && results.length > 0) {
-                                const keys = results.filter(result => result.isDeleted()).map(result => result.getUserStoreKey());
-                                const msg = i18n(`notify.delete.userstore.${keys.length === 1 ? 'single' : 'multiple'}`, keys.join(', '));
+                            if (json.results && json.results.length > 0) {
+                                let key = json.results[0].userStoreKey;
 
-                                api.notify.showFeedback(msg);
+                                api.notify.showFeedback(i18n('notify.delete.userstore', key));
                                 api.security.UserItemDeletedEvent.create().setUserStores(userStoreItems).build().fire();
                             }
                         });
