008feae97fd05b744c169d4e3c9a4963d1a043d8
Siarhei Hauruseu <sig@enonic.com>
2018-10-18 19:14:40 +0300

    ui-tests minor changes

--- testing/test-data/selenium-server.sh
+++ testing/test-data/selenium-server.sh
@@ -36,7 +36,7 @@ init() {
 }
 
 run() {
-    exec "$JAVACMD" -Dwebdriver.chrome.driver=test-data/chromedriver -jar test-data/selenium-server-standalone-3.4.0.jar
+    java -Dwebdriver.chrome.driver=test-data/chromedriver -jar test-data/selenium-server-standalone-3.4.0.jar
 }
 
 main() {
