17f54ba71700561a94c0232ecc75674306f162e3
Viachaslau Bradnitski <vbr@enonic.com>
2019-01-04 16:10:01 +0300

    User store / ID Provider refactoring - Renaming #184 AuthDescriptor -> IdProviderDescriptor

--- src/main/java/com/enonic/xp/app/users/lib/auth/GetIdProviderModeHandler.java
+++ src/main/java/com/enonic/xp/app/users/lib/auth/GetIdProviderModeHandler.java
@@ -3,18 +3,17 @@ package com.enonic.xp.app.users.lib.auth;
 import java.util.function.Supplier;
 
 import com.enonic.xp.app.ApplicationKey;
-import com.enonic.xp.auth.AuthDescriptor;
-import com.enonic.xp.auth.AuthDescriptorService;
+import com.enonic.xp.idprovider.IdProviderDescriptor;
+import com.enonic.xp.idprovider.IdProviderDescriptorService;
 import com.enonic.xp.script.bean.BeanContext;
 import com.enonic.xp.script.bean.ScriptBean;
-import com.enonic.xp.security.UserStore;
 
 public final class GetIdProviderModeHandler
     implements ScriptBean
 {
     private ApplicationKey applicationKey;
 
-    private Supplier<AuthDescriptorService> authDescriptorService;
+    private Supplier<IdProviderDescriptorService> idProviderDescriptorService;
 
     public void setApplicationKey( final String applicationKey )
     {
@@ -23,13 +22,13 @@ public final class GetIdProviderModeHandler
 
     public String getIdProviderMode()
     {
-        final AuthDescriptor authDescriptor = authDescriptorService.get().getDescriptor( applicationKey );
-        return authDescriptor == null ? null : authDescriptor.getMode().toString();
+        final IdProviderDescriptor idProviderDescriptor = idProviderDescriptorService.get().getDescriptor( applicationKey );
+        return idProviderDescriptor == null ? null : idProviderDescriptor.getMode().toString();
     }
 
     @Override
     public void initialize( final BeanContext context )
     {
-        authDescriptorService = context.getService( AuthDescriptorService.class );
+        idProviderDescriptorService = context.getService( IdProviderDescriptorService.class );
     }
 }
