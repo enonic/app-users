fbafeb5ebbc0094c3b4ae453fc5abf7c17bcfea3
Viachaslau Bradnitski <vbr@enonic.com>
2019-01-10 17:02:46 +0300

    User store / ID Provider refactoring - Renaming #184 UserStore -> IdProvider (phrases )

--- src/main/resources/assets/js/app/wizard/IdProviderAccessSelector.ts
+++ src/main/resources/assets/js/app/wizard/IdProviderAccessSelector.ts
@@ -13,7 +13,7 @@ export class IdProviderAccessSelector
         {value: IdProviderAccess.READ, name: 'Read'},
         {value: IdProviderAccess.CREATE_USERS, name: 'Create Users'},
         {value: IdProviderAccess.WRITE_USERS, name: 'Write Users'},
-        {value: IdProviderAccess.ID_PROVIDER_MANAGER, name: 'User Store Manager'},
+        {value: IdProviderAccess.ID_PROVIDER_MANAGER, name: 'Id Provider Manager'},
         {value: IdProviderAccess.ADMINISTRATOR, name: 'Administrator'}
     ];
 
