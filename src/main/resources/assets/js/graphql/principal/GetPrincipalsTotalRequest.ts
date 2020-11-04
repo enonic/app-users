d50783d45748b6180c6d33708662fc95c2f0fab2
Aliaksandr Shklianko <asl@enonic.com>
2020-11-04 12:31:48 +0300

    Sort new leaf nodes by display name in alphabetical order #258
    -also correctly showing expand/collapse icon

---
+++ src/main/resources/assets/js/graphql/principal/GetPrincipalsTotalRequest.ts
@@ -0,0 +1,47 @@
+import {ListGraphQlRequest} from '../ListGraphQlRequest';
+import {PrincipalType} from 'lib-admin-ui/security/PrincipalType';
+import {IdProviderKey} from 'lib-admin-ui/security/IdProviderKey';
+
+export class GetPrincipalsTotalRequest
+    extends ListGraphQlRequest<number> {
+
+    private types: PrincipalType[];
+    private idProviderKey: IdProviderKey;
+
+    setTypes(types: PrincipalType[]): GetPrincipalsTotalRequest {
+        this.types = types;
+        return this;
+    }
+
+    setIdProviderKey(key: IdProviderKey): GetPrincipalsTotalRequest {
+        this.idProviderKey = key;
+        return this;
+    }
+
+    getVariables(): { [key: string]: any } {
+        let vars = super.getVariables();
+        if (this.types && this.types.length > 0) {
+            vars['types'] = this.types.map(type => PrincipalType[type]);
+        }
+        if (this.idProviderKey) {
+            vars['idprovider'] = this.idProviderKey.toString();
+        }
+
+        return vars;
+    }
+
+    getQuery(): string {
+        return `query($idprovider: String, $types: [PrincipalType], $start: Int, $count: Int, $sort: String) {
+                  principalsConnection (idprovider: $idprovider, types: $types, start: $start, count: $count, sort: $sort) {
+                        totalCount
+                    }
+                }`;
+    }
+
+    sendAndParse(): Q.Promise<number> {
+        return this.query().then((response: any) => {
+            const data = response.principalsConnection;
+            return data.totalCount;
+        });
+    }
+}
