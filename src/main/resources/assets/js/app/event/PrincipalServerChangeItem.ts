0063c3c13822f33b4957055fbb8d1642acdc6629
Alan Semenov <ase@enonic.com>
2021-10-07 08:05:54 +0200

    Codacy

--- src/main/resources/assets/js/app/event/PrincipalServerChangeItem.ts
+++ src/main/resources/assets/js/app/event/PrincipalServerChangeItem.ts
@@ -15,10 +15,10 @@ export class PrincipalServerChangeItem
         }
 
         const fullPathWithRoot: NodePath = NodePath.create().fromString(path).build();
-        const pathNoRoot: NodePath = <NodePath>fullPathWithRoot
+        const pathNoRoot: NodePath = fullPathWithRoot
             .newBuilder()
             .setElements(fullPathWithRoot.getElements().slice(1))
-            .build();
+            .build() as NodePath;
 
         return pathNoRoot;
     }
