00724dca0d8a985490186605befa815a88a76e87
Aliaksandr Shklianko <asl@enonic.com>
2022-02-21 17:15:22 +0300

    -codacyBring keyboard shortcuts in line with Content Studio's shortcuts #648

--- src/main/resources/assets/js/app/browse/action/NewPrincipalAction.ts
+++ src/main/resources/assets/js/app/browse/action/NewPrincipalAction.ts
@@ -8,7 +8,7 @@ import {i18n} from 'lib-admin-ui/util/Messages';
 export class NewPrincipalAction extends Action {
 
     constructor(grid: UserItemsTreeGrid) {
-        super(i18n('action.new'), 'mod+alt+n');
+        super(i18n('action.new'), 'alt+n');
         this.setEnabled(false);
         this.onExecuted(() => {
             const principals: UserTreeGridItem[] = grid.getSelectedDataList();
