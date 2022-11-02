13d46a65d416e6c49c68ae3d39e535b6ba190825
ashklianko <asl@enonic.com>
2022-11-02 10:44:53 +0100

    It shouldn't be possible to unassign system.administrator role from su user #1227

--- src/main/resources/assets/js/app/wizard/MembersWizardStepForm.ts
+++ src/main/resources/assets/js/app/wizard/MembersWizardStepForm.ts
@@ -1,21 +1,26 @@
 import {Principal} from '@enonic/lib-admin-ui/security/Principal';
 import {PrincipalKey} from '@enonic/lib-admin-ui/security/PrincipalKey';
 import {PrincipalType} from '@enonic/lib-admin-ui/security/PrincipalType';
-import {PrincipalComboBox} from '@enonic/lib-admin-ui/ui/security/PrincipalComboBox';
+import {
+    PrincipalComboBox,
+    PrincipalComboBoxBuilder,
+    PrincipalSelectedOptionsView
+} from '@enonic/lib-admin-ui/ui/security/PrincipalComboBox';
 import {FormItem, FormItemBuilder} from '@enonic/lib-admin-ui/ui/form/FormItem';
 import {i18n} from '@enonic/lib-admin-ui/util/Messages';
 import {Members} from '../principal/Members';
 import {ObjectHelper} from '@enonic/lib-admin-ui/ObjectHelper';
 import {UserItemWizardStepForm} from './UserItemWizardStepForm';
-import {PrincipalLoader as BasePrincipalLoader} from '@enonic/lib-admin-ui/security/PrincipalLoader';
 import {PrincipalLoader} from '../principal/PrincipalLoader';
+import {Option} from '@enonic/lib-admin-ui/ui/selector/Option';
+import * as Q from 'q';
+import {SelectedOption} from '@enonic/lib-admin-ui/ui/selector/combobox/SelectedOption';
+import {RoleKeys} from '@enonic/lib-admin-ui/security/RoleKeys';
 
 export class MembersWizardStepForm
     extends UserItemWizardStepForm {
 
-    private principals: PrincipalComboBox;
-
-    private loader: BasePrincipalLoader;
+    private principalCombobox: MembersPrincipalCombobox;
 
     constructor() {
         super('membership-wizard-step-form');
@@ -24,32 +29,40 @@ export class MembersWizardStepForm
     protected initElements(): void {
         super.initElements();
 
-        this.loader = new PrincipalLoader()
-                .setAllowedTypes([PrincipalType.GROUP, PrincipalType.USER])
-                .skipPrincipals([PrincipalKey.ofAnonymous()])
-                .setUseDataPreLoad(true);
-        this.principals = <PrincipalComboBox>(PrincipalComboBox.create().setLoader(this.loader).build());
+        const loader: PrincipalLoader = <PrincipalLoader>new PrincipalLoader()
+            .setAllowedTypes([PrincipalType.GROUP, PrincipalType.USER])
+            .skipPrincipals([PrincipalKey.ofAnonymous()])
+            .setUseDataPreLoad(true);
+
+        const builder: PrincipalComboBoxBuilder = <PrincipalComboBoxBuilder>PrincipalComboBox.create()
+            .setLoader(loader)
+            .setSelectedOptionsView(new MembersPrincipalSelectedOptionsView());
+        this.principalCombobox = new MembersPrincipalCombobox(builder);
     }
 
     protected createFormItems(): FormItem[] {
-        const principalsFormItem: FormItem = new FormItemBuilder(this.principals).setLabel(i18n('field.members')).build();
+        const principalsFormItem: FormItem = new FormItemBuilder(this.principalCombobox).setLabel(i18n('field.members')).build();
         return [principalsFormItem];
     }
 
     layout(principal: Members): void {
-        if (this.principals.isDirty()) {
+        if (RoleKeys.isAdmin(principal.getKey())) {
+            this.principalCombobox.setReadOnlyItems([PrincipalKey.ofSU()]);
+        }
+
+        if (this.principalCombobox.isDirty()) {
             if (ObjectHelper.arrayEquals(this.getMembersKeys(), principal.getMembers())) {
-                this.principals.resetBaseValues();
+                this.principalCombobox.resetBaseValues();
             }
         } else {
-            this.loader.skipPrincipal(principal.getKey());
+            this.getLoader().skipPrincipal(principal.getKey());
             const value: string = principal.getMembers().map((key: PrincipalKey) => key.toString()).join(';');
-            this.principals.setValue(value);
+            this.principalCombobox.setValue(value);
         }
     }
 
     getMembers(): Principal[] {
-        return this.principals.getSelectedDisplayValues();
+        return this.principalCombobox.getSelectedDisplayValues();
     }
 
     getMembersKeys(): PrincipalKey[] {
@@ -57,11 +70,11 @@ export class MembersWizardStepForm
     }
 
     giveFocus(): boolean {
-        return this.principals.giveFocus();
+        return this.principalCombobox.giveFocus();
     }
 
-    getLoader(): BasePrincipalLoader {
-        return this.loader;
+    getLoader(): PrincipalLoader {
+        return <PrincipalLoader>this.principalCombobox.getLoader();
     }
 
     doRender(): Q.Promise<boolean> {
@@ -70,5 +83,50 @@ export class MembersWizardStepForm
 
             return rendered;
         });
+    }
+}
+
+class MembersPrincipalCombobox
+    extends PrincipalComboBox {
+
+    private readOnlyItems: PrincipalKey[] = [];
+
+    protected createOption(p: Principal, readOnly?: boolean): Option<Principal> {
+        return super.createOption(p, readOnly || this.isReadOnlyItem(p.getKey()));
+    }
+
+    setReadOnlyItems(items: PrincipalKey[]): void {
+        this.readOnlyItems = items || [];
+
+        (<MembersPrincipalSelectedOptionsView>this.getSelectedOptionView()).setReadOnlyItems(items);
+    }
+
+    private isReadOnlyItem(key: PrincipalKey): boolean {
+        return this.readOnlyItems.some((item: PrincipalKey) => item.equals(key));
+    }
+}
+
+class MembersPrincipalSelectedOptionsView
+    extends PrincipalSelectedOptionsView {
+
+    private readOnlyItems: PrincipalKey[] = [];
+
+    createSelectedOption(option: Option<Principal>): SelectedOption<Principal> {
+        const selectedOption = super.createSelectedOption(option);
+
+        if (this.isReadOnlyItem(option)) {
+            selectedOption.getOptionView().setReadonly(true);
+        }
+
+        return selectedOption;
+    }
+
+    setReadOnlyItems(items: PrincipalKey[]): void {
+        this.readOnlyItems = items || [];
+    }
+
+    private isReadOnlyItem(option: Option<Principal>): boolean {
+        const key: PrincipalKey = option.getDisplayValue().getKey();
+        return this.readOnlyItems.some((item: PrincipalKey) => item.equals(key));
     }
 }
