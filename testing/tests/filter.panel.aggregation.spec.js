aa81f0aa7693f495a498d00a7105414e7595b701
sgauruseu <sig@eninic.com>
2022-11-23 13:45:38 +0300

    fix fo failed ui-test

--- testing/tests/filter.panel.aggregation.spec.js
+++ testing/tests/filter.panel.aggregation.spec.js
@@ -13,7 +13,7 @@ describe('filter.panel.aggregation.spec Principal Aggregation specification', fu
         webDriverHelper.setupBrowser();
     }
 
-    it("GIVEN 'Principal Filter Panel' is opened WHEN 'User' aggregation has been clicked THEN Roles and Id Provider should not be displayed",
+    it("GIVEN 'User' aggregation checkbox is checked WHEN the checkbox has been clicked THEN grid returns to the initial state",
         async () => {
             let browseFilterPanel = new BrowseFilterPanel();
             let userBrowsePanel = new UserBrowsePanel();
@@ -21,10 +21,32 @@ describe('filter.panel.aggregation.spec Principal Aggregation specification', fu
             await testUtils.openFilterPanel();
             //2. Click on 'User' aggregation checkbox:
             await browseFilterPanel.clickOnUserAggregation();
-            //3. user-items should be filtered: Roles and System ID provider folders should not be displayed:
-            await userBrowsePanel.waitForItemNotDisplayed('roles');
-            await userBrowsePanel.waitForItemNotDisplayed('/system');
+            //3. Verify that user-items should be filtered: 'Roles' and 'System ID provider' folders should not be displayed:
+            await userBrowsePanel.waitForItemByDisplayNameNotDisplayed('Roles');
+            await userBrowsePanel.waitForItemByDisplayNameNotDisplayed('System Id Provider');
+            //4. Click on 'User' aggregation checkbox:
+            await browseFilterPanel.clickOnUserAggregation();
+            await testUtils.saveScreenshot('returns_to_initial');
+            //5. Verify that 'System Id Provider' and Roles folders get visible again:
+            await userBrowsePanel.waitForItemByDisplayNameDisplayed('System Id Provider');
+            await userBrowsePanel.waitForItemByDisplayNameDisplayed('Roles');
+        });
+
+    it("GIVEN 'Principal Filter Panel' is opened WHEN 'User' aggregation has been clicked THEN Roles and Id Provider should not be displayed",
+        async () => {
+            let browseFilterPanel = new BrowseFilterPanel();
+            let userBrowsePanel = new UserBrowsePanel();
+            //1. Open Filter Panel:
+            await testUtils.openFilterPanel();
+            //2. Verify that 'System Id Provider' folder is displayed by initially:
+            await userBrowsePanel.waitForItemByDisplayNameDisplayed('System Id Provider');
+            //3. Click on 'User' aggregation checkbox:
+            await browseFilterPanel.clickOnUserAggregation();
+            //4. Verify that user-items should be filtered: 'Roles' and 'System ID provider' folders should not be displayed:
+            await userBrowsePanel.waitForItemByDisplayNameNotDisplayed('Roles');
+            await userBrowsePanel.waitForItemByDisplayNameNotDisplayed('System Id Provider');
             await testUtils.saveScreenshot('aggregation_in_users');
+            //5. Verify that SU is present in the filtered grid
             let isDisplayed = await userBrowsePanel.isItemDisplayed('/system/users/su');
             assert.isTrue(isDisplayed, "SU should be displayed in the filtered panel");
         });
@@ -38,7 +60,7 @@ describe('filter.panel.aggregation.spec Principal Aggregation specification', fu
             await filterPanel.clickOnRoleAggregation();
             await testUtils.saveScreenshot('aggregation_in_role');
             //Verify that 'System Id Provider' is not displayed in the filtered grid:
-            await userBrowsePanel.waitForItemNotDisplayed('/system');
+            await userBrowsePanel.waitForItemByDisplayNameNotDisplayed('System Id Provider');
             //Verify that 'Users Administrator' role is displayed:
             let isDisplayed = await userBrowsePanel.isItemDisplayed('/roles/system.user.admin');
             assert.isTrue(isDisplayed, "expected role should be displayed in the filtered panel");
@@ -49,12 +71,13 @@ describe('filter.panel.aggregation.spec Principal Aggregation specification', fu
             let filterPanel = new BrowseFilterPanel();
             let userBrowsePanel = new UserBrowsePanel();
             await testUtils.openFilterPanel();
-            //Click on 'Provider' checkbox:
+            //1. Click on 'Provider' checkbox:
             await filterPanel.clickOnIdProviderAggregation();
-            await userBrowsePanel.waitForItemNotDisplayed('roles');
             await testUtils.saveScreenshot('aggregation_in_provider');
-            let isDisplayed = await userBrowsePanel.isItemDisplayed('/system');
-            assert.isTrue(isDisplayed, "roles are not displayed but System folder should be displayed in the filtered panel");
+            //2. 'System Id Provider' folder should be displayed in the filtered panel
+            await userBrowsePanel.waitForItemByDisplayNameDisplayed('System Id Provider');
+            //3. Roles folder should not be displayed but
+            await userBrowsePanel.waitForItemByDisplayNameNotDisplayed('Roles');
         });
 
     beforeEach(() => testUtils.navigateToUsersApp());
