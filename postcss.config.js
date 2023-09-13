d5e61d629dea402c8a7840117bf715b91f0b9dda
Christian Westgaard <comlock@gmail.com>
2023-09-13 09:20:03 +0200

    wip #1817 jquery problems

--- postcss.config.js
+++ postcss.config.js
@@ -10,9 +10,7 @@ const plugins = Object.assign(
         'postcss-url': {
             basePath: path.resolve('src/main/resources/assets/icons'),
             assetsPath: '../icons/',
-            url: 'copy' // Remember to skip woff and woff2 files in build.gradle
-            // url: 'inline' // base64 data url inlines the fonts?
-            // url: 'rebase' // only manipulates the urls?
+            url: 'copy'
         },
     },
     isProd ? {cssnano: {}} : {}
