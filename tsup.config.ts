14a344a4c9d3a47ce759414bfbea2015a80a9d37
Christian Westgaard <comlock@gmail.com>
2023-09-14 14:08:17 +0200

    wip #1817 webpack specific require.context()

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,58 +1,18 @@
-import { globalExternals } from "@fal-works/esbuild-plugin-global-externals";
-// import GlobalsPlugin from "esbuild-plugin-globals";
-import { defineConfig } from 'tsup';
-// import externalGlobalPlugin from 'esbuild-plugin-external-global';
+import type { Options } from './tsup';
 
-export default defineConfig(() => {
-    return {
-        bundle: true,
-        dts: false, // d.ts files are use useless at runtime
-        entry: {
-            'js/app-users-bundle': 'src/main/resources/assets/js/main.ts',
-            'js/crypto-worker': 'src/main/resources/assets/js/worker/RSAKeysWorker.ts',
-        },
-        // esbuildOptions(options, context) {
-        //     options.external = [
-        //         'jquery'
-        //     ]
-        // },
-        esbuildPlugins: [
-            // GlobalsPlugin({
-            //     // 'jquery': 'jQuery',
-            //     'jquery': '$'
-            // }),
-            globalExternals({
-                'jquery': '$'
-                // 'jquery': 'jQuery'
-            })
-            // externalGlobalPlugin({
-            //     // 'react': 'window.React',
-            //     // 'react-dom': 'window.ReactDOM',
-            //     'jQuery': '$' // It seems jquery is available as $ in the global scope
-            //   })
-        ],
-        // external: [
-        //     // This will leave require('jquery') as is in the bundle
-        //     // causes: Uncaught ReferenceError: require is not defined
-        //     'jquery'
-        // ],
-        format: [
-            'cjs'
-        ],
-        minify: process.env.NODE_ENV !== 'development',
-        noExternal: [ // Same as dependencies in package.json
-            /@enonic\/lib-admin-ui/,
-            'hasher',
-            'jquery', // This will bundle jQuery into the bundle
-            'nanoid',
-            'owasp-password-strength-test',
-            'q'
-        ],
-        outDir: 'build/resources/main/assets',
-        platform: 'browser',
-        silent: ['QUIET', 'WARN'].includes(process.env.LOG_LEVEL_FROM_GRADLE||''),
-        // splitting: false,
-        sourcemap: process.env.NODE_ENV === 'development',
-        tsconfig: 'src/main/resources/assets/tsconfig.json',
-    };
+import { defineConfig } from 'tsup';
+import { DIR_DST } from './tsup/constants';
+
+
+export default defineConfig((options: Options) => {
+    if (options.d === DIR_DST) {
+        return import('./tsup/server').then(m => m.default());
+	}
+    if (options.d === 'build/resources/main/assets') {
+        return import('./tsup/assets').then(m => m.default());
+	}
+    if (options.d === 'build/resources/main/static') {
+        return import('./tsup/static').then(m => m.default());
+    }
+    throw new Error(`Unconfigured directory:${options.d}!`)
 });
