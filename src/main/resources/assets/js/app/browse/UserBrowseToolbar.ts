a3d75395adc08767b8d1c12191562bad9d2466ac
ashklianko <asl@enonic.com>
2024-09-30 00:22:56 +0200

    slick rebase

--- src/main/resources/assets/js/app/browse/UserBrowseToolbar.ts
+++ src/main/resources/assets/js/app/browse/UserBrowseToolbar.ts
@@ -1,11 +1,12 @@
 import {Toolbar, ToolbarConfig} from '@enonic/lib-admin-ui/ui/toolbar/Toolbar';
+import {UserTreeGridActions} from './UserTreeGridActions';
 
 export class UserBrowseToolbar
     extends Toolbar<ToolbarConfig> {
 
-    constructor() {
-        super({
-            className: 'user-browse-toolbar'
-        });
+    constructor(actions: UserTreeGridActions) {
+        super();
+        this.addClass('user-browse-toolbar');
+        this.addActions(actions.getAllActions());
     }
 }
