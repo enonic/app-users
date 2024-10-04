0cb17337829ef64b17478a325d758018b839d256
ashklianko <asl@enonic.com>
2024-10-04 11:21:25 +0200

    ID Provider config dialog - PrincipalComboBox is empty #2120

--- src/main/resources/assets/js/app/inputtype/selector/PrincipalSelector.ts
+++ src/main/resources/assets/js/app/inputtype/selector/PrincipalSelector.ts
@@ -1,14 +1,12 @@
 import {PrincipalSelector as BasePrincipalSelector} from '@enonic/lib-admin-ui/form/inputtype/principal/PrincipalSelector';
-import {PrincipalLoader as BasePrincipalLoader} from '@enonic/lib-admin-ui/security/PrincipalLoader';
-import {PrincipalLoader} from '../../principal/PrincipalLoader';
 import {InputTypeName} from '@enonic/lib-admin-ui/form/InputTypeName';
 import {UrlHelper} from '../../../util/UrlHelper';
 
 export class PrincipalSelector
     extends BasePrincipalSelector {
 
-    protected createLoader(): BasePrincipalLoader {
-        return new PrincipalLoader(UrlHelper.getRestUri(''));
+    protected getPostfixUri(): string {
+        return UrlHelper.getRestUri('');
     }
 
     static getName(): InputTypeName {
